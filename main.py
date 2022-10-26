af46fe0844a59b9c9ce8d62594bebbfada9af3b7
Nathan Brinton <nab.natethegreat@gmail.com>
2022-10-26 13:15:23 -0600

    - Refactored to use the BeautifulSoup parser in order to circumvent the malformed XML files. - Added sluggify to make sure that the generated filenames are filename-safe. - Slightly re-sized and moved the slide textboxes. - Created variables for the file locations and names instead of hard-coded values. - Removed un-used imports.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from pptx import Presentation
-from pptx.util import Inches, Mm, Cm
+from pptx.util import Inches
 from pptx.enum.text import PP_PARAGRAPH_ALIGNMENT as PP_ALIGN
 from pptx.util import Pt
 from pptx.dml.color import RGBColor
-import xml.etree.ElementTree as et
-
+from bs4 import BeautifulSoup
+from slugify import slugify
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -30,7 +30,7 @@ def add_verse_slide(pres, verse):
         if i != len(verse_paragraphs) - 1:
             verse_text += '\n'
 
-    slide.shapes.add_textbox(Inches(0.125), Inches(3), Inches(13.125), Inches(4))
+    slide.shapes.add_textbox(Inches(0.125), Inches(2.5), Inches(13.125), Inches(5))
 
     for shape in slide.shapes:
         if not shape.has_text_frame:
@@ -75,7 +75,11 @@ def add_verse_slide(pres, verse):
 if __name__ == '__main__':
     print('Generating powerpoint presentations...')
 
-    src_dir = 'examples'
+    # src_dir = 'songs'
+    src_dir = 'songs'
+    output_dir = 'powerpoints'
+    base_pptx = 'base.pptx'
+
     hymn_xml_files = os.listdir(src_dir)
 
     for f in hymn_xml_files:
@@ -84,72 +88,58 @@ if __name__ == '__main__':
 
         print(f'\tCreating pptx for file: {os.path.join(src_dir, f)}')
 
-        tree = et.parse(f'{os.path.join(src_dir, f)}')
-        root = tree.getroot()
+        with open(os.path.join(src_dir, f)) as fp:
+            soup = BeautifulSoup(fp, 'lxml-xml')
 
-        title = None
-        song_number = None
-        chorus = None
-        verses = []
-        author = None
+            title = soup.find('Title').text.strip()
+            song_number = soup.find('SongNumber').text.strip()
+            verses = list(map(lambda v: v.text.strip(), soup.findAll('Text')))
+            author = soup.find('Author').text.strip()
 
-        for child in root:
-            if child.tag == 'Title':
-                title = child.text
-            elif child.tag == 'SongNumber':
-                song_number = child.text
-            elif child.tag == 'Text' and child.attrib['section'] == 'Chorus':
-                chorus = child.text.strip()
-            elif child.tag == 'Text' and child.attrib['section'] != 'Chorus':
-                verses.append(child.text.strip())
-            elif child.tag == 'Author':
-                author = child.text.strip()
+            # Create a pptx file using the base pptx so we can use its theme
+            pres = Presentation(base_pptx)
 
-        # Create a pptx file using the base pptx so we can use its theme
-        pres = Presentation('base.pptx')
+            # Delete the first slide (yuck). See https://stackoverflow.com/a/70969310
+            for i in range(len(pres.slides) - 1, -1, -1):
+                rId = pres.slides._sldIdLst[i].rId
+                pres.part.drop_rel(rId)
+                del pres.slides._sldIdLst[i]
 
-        # Delete the first slide (yuck). See https://stackoverflow.com/a/70969310
-        for i in range(len(pres.slides) - 1, -1, -1):
-            rId = pres.slides._sldIdLst[i].rId
-            pres.part.drop_rel(rId)
-            del pres.slides._sldIdLst[i]
+            for verse in verses:
+                add_verse_slide(pres, verse)
 
-        for verse in verses:
-            add_verse_slide(pres, verse)
-            add_verse_slide(pres, chorus)
+            # Add the last slide
 
-        # Add the last slide
+            # Blank layout
+            layout = pres.slide_layouts[6]
 
-        # Blank layout
-        layout = pres.slide_layouts[6]
+            end_slide = pres.slides.add_slide(layout)
+            end_slide.shapes.add_textbox(Inches(0.125), Inches(7), Inches(13.125), Inches(0.5))
 
-        end_slide = pres.slides.add_slide(layout)
-        end_slide.shapes.add_textbox(Inches(0.125), Inches(7), Inches(13.125), Inches(0.5))
+            for shape in end_slide.shapes:
+                if not shape.has_text_frame:
+                    continue
 
-        for shape in end_slide.shapes:
-            if not shape.has_text_frame:
-                continue
+                text_frame = shape.text_frame
 
-            text_frame = shape.text_frame
+                # Clear out the default empty paragraph
+                text_frame.clear()
+                text_frame.left = Inches(0)
+                text_frame.width = Inches(13)
+                p = text_frame.paragraphs[0]
+                p.alignment = PP_ALIGN.LEFT
+                p.level = 0
+                # p.text = verse_text
 
-            # Clear out the default empty paragraph
-            text_frame.clear()
-            text_frame.left = Inches(0)
-            text_frame.width = Inches(13)
-            p = text_frame.paragraphs[0]
-            p.alignment = PP_ALIGN.LEFT
-            p.level = 0
-            # p.text = verse_text
+                run = p.add_run()
+                run.text = author
+                font = run.font
+                font.name = 'Calibri'
+                font.size = Pt(14)
+                font.color.rgb = RGBColor(255, 255, 255)
 
-            run = p.add_run()
-            run.text = author
-            font = run.font
-            font.name = 'Calibri'
-            font.size = Pt(14)
-            font.color.rgb = RGBColor(255, 255, 255)
-
-        filename = f'{song_number.zfill(3)} {title}.pptx'
-        filepath = os.path.join('powerpoints', filename)
-        pres.save(filepath)
+            filename = slugify(f'{song_number.zfill(3)} {title}')
+            filepath = os.path.join(output_dir, filename)
+            pres.save(f'{filepath}.pptx')
 
     print('Done!')
